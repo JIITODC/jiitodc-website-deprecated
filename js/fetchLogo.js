42a5a3341bd1a8c99542261228834bbc669d9e45
prox07 <19point9@gmail.com>
2018-10-19 17:19:04 +0530

    async fix

--- js/fetchLogo.js
+++ js/fetchLogo.js
@@ -5,11 +5,10 @@ const URL = 'https://preview.ibb.co/n0YRaf/logo.jpg'
 fetch(URL).then(
     data => {
         sessionStorage.setItem('logoURL',data.url)
+        let source = sessionStorage.getItem('logoURL')
+        document.getElementById('logo').href = source;
     }
 ).catch(console.error())
 
-let source = sessionStorage.getItem('logoURL')
-
-document.getElementById('logo').href = source;
-
+console.log('source fetched!')
 }
