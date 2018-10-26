d743b3bc18d58e2f98344548a740dbd499095c6a
Jonathan Sexton <joncsexton@.com>
2018-10-26 17:03:43 -0500

    Menu button now appears at righr break point

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,24 @@
-var title = document.getElementById('head-text');
+var title = document.getElementById("head-text");
 
 var typewriter = new Typewriter(title, {
-    loop: true
+  loop: true,
 });
 
-typewriter.typeString('JODC')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('JIIT Open Dev Circle ')
+typewriter
+  .typeString("JODC")
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString("JIIT Open Dev Circle ")
 
-    .start();
-   
+  .start();
+
 const closeButton = document.getElementById("close");
-const topNav = document.querySelector('.topnav');
+const topNav = document.querySelector(".topnav");
 
-closeButton.addEventListener("mouseenter", function () {
+closeButton.addEventListener("click", () => {
+  if (topNav.style.visibility === "hidden") {
     topNav.style.visibility = "visible";
+  } else {
+    topNav.style.visibility = "hidden";
+  }
 });
-
-closeButton.addEventListener("mouseleave", () => (topNav.style.visibility = "hidden"));
